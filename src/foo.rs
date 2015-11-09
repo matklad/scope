a549035a479430b38944bae1d51e7b72b18418fd
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-11-09 15:59:50 +0300

    add example

--- src/foo.rs
+++ src/foo.rs
@@ -1,4 +1,13 @@
+pub fn bar() {
+    println!("I'm bar");
+}
+
 pub fn fn_foo() {
     println!("calling function using the crate scope");
     ::fn_main();
+
+//     Will not compile
+//     ::bar();
+
+    ::foo::bar();
 }
